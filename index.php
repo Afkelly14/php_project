69e918a7577e8cb90c8d24389a3cabdbbe658944
Alex_Kelly <afkelly14@Gmail.com>
2021-01-27 16:36:22 -0500

    re-doing calculator

--- index.php
+++ index.php
@@ -8,13 +8,41 @@
 <body>
     <form action="functions.php" method="get">
         <input type="text" name="num01" placeholder="Number 1">
-        <select name="oper">
-            <label>Choose operation!</label>
-            <option value="add">Add</option>
-            <option value='sub'>Subtract</option>
-        </select>
         <input type="text" name="num02" placeholder="Number 2">
-        <button type="submit">Calculate!</button>
+        <select name="operator">
+            <option>None</option>
+            <option>Subtract</option>
+            <option>Multiply</option>
+            <option>Divide</option>
+        </select>
+        <br/>
+        <button name="submit" value="submit" type="submit">Calculate</button>
     </form>
+<p>The answer is: </p>
+    <?php
+        if(isset($_GET['submit'])) {
+            $result1 = $_GET['num01'];
+            $result2 = $_GET['num02'];
+            $oper = $_GET['oper'];
+            switch ($oper) {
+                case "add":
+                    $sum = $result1 + $result2;
+                    break;
+                case "sub":
+                    $sum = $result1 - $result2;
+                    break;
+                case "multiply":
+                    $sum = $result1 * $result2;
+                    break;
+                case "divide":
+                    $sum = $result1 / $result2;
+                    break;
+                default:
+                    $sum = "There was an error";
+                    break;
+        }
+            }
+        
+    ?>
 </body>
 </html>
